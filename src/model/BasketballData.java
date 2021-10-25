694c9f21762d3e3d96711714b793f842e5866c0a
Luis Miguel <luismiguelossaarias05@gmail.com>
2021-10-24 22:48:36 -0500

    Some changes

--- src/model/BasketballData.java
+++ src/model/BasketballData.java
@@ -1,16 +1,31 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class BasketballData {
 	
 	public BasketballData() {}
 	
 	public void searchPlayer() {}
 	
-	public void importData() {}
+	public void loadData() {}
 
 	public void saveData() {}
 	
-	public void loadData() {}
-	
-	public void exportData() {}
+	public void importData(String fileName) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		String line = br.readLine();
+		
+		while(line != null) {
+			String[] players = line.split(";");
+			
+			//createPlayer(players);
+			
+			line = br.readLine();
+		}
+		br.close();
+	}
 }
