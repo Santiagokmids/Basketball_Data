aa605ca980f6dd381c8dc563fbbd9c2f5721eaeb
Luis Miguel <luismiguelossaarias05@gmail.com>
2021-10-24 22:28:07 -0500

    The new stage to modify the players has been added

--- src/ui/BasketballDataGUI.java
+++ src/ui/BasketballDataGUI.java
@@ -8,10 +8,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.effect.DropShadow;
@@ -20,12 +22,43 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import model.BasketballData;
 import model.Players;
 
 public class BasketballDataGUI {
 
 	//public BasketballData basketData;
+	
+
+    @FXML
+    private TextField lblName;
+
+    @FXML
+    private TextField lblLastName;
+
+    @FXML
+    private TextField lblAge;
+
+    @FXML
+    private TextField lblBlock;
+
+    @FXML
+    private TextField lblAssistance;
+
+    @FXML
+    private TextField lblTeam;
+
+    @FXML
+    private TextField lblTheft;
+
+    @FXML
+    private TextField lblBounce;
+
+    @FXML
+    private TextField lblPoints;
+
 	
 	@FXML
     private TableColumn<Players, String> tcName;
@@ -257,7 +290,7 @@ public class BasketballDataGUI {
 	    	
 	    	Image title = new Image("/images/listPlayersT.png");
 			titleAnchPlayers.setImage(title);
-			inicializateTableView();
+			//inicializateTableView();
 			break;
 		case "  Agregar":
 			
@@ -424,6 +457,22 @@ public class BasketballDataGUI {
 	@FXML
     public void btnModify(ActionEvent event) {
 		
+		
+		
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("modify-pane.fxml"));
+			
+			Parent root = loader.load();
+			
+			Scene scene = new Scene(root);
+			Stage stage = new Stage();
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.setScene(scene);
+			stage.showAndWait();
+			
+		} catch (IOException e) {
+		}
+		/**
 		Players players = (Players) this.tvPlayers.getSelectionModel().getSelectedItem();
 		
 		if(players == null) {
@@ -433,14 +482,17 @@ public class BasketballDataGUI {
 			alert.setContentText("Es necesario seleccionar un jugador");
 			alert.showAndWait();
 		}else {
-			try {
-				FXMLLoader loader = new FXMLLoader(getClass().getResource("modify-pane.fxml"));
-				
-				Parent root = loader.load();
-				
-				
-			} catch (IOException e) {
-			}
-		}
+			
+		}*/
+    }
+	
+	@FXML
+    public void btnModifyExit(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void btnSave(ActionEvent event) {
+
     }
 } 
