7f35522612fd5aedac14997ab1666540aec0b12b
Luis Miguel <luismiguelossaarias05@gmail.com>
2021-10-25 23:09:53 -0500

    Some changes

--- src/dataStructures/AVLTree.java
+++ src/dataStructures/AVLTree.java
@@ -1,18 +1,18 @@
 package dataStructures;
 
-public class AVLTree<K, V, F> implements IAVLTree<K, V, F>{
+public class AVLTree<K, V, F, H> implements IAVLTree<K, V, F, H>{
 	
-	private NodeAVLTree<K, V, F> root;
+	private NodeAVLTree<K, V, F, H> root;
 	
 	public AVLTree() {
 		createTree();
 	}
 
-	public NodeAVLTree<K, V, F> getRoot() {
+	public NodeAVLTree<K, V, F, H> getRoot() {
 		return root;
 	}
 
-	public void setRoot(NodeAVLTree<K, V, F> root) {
+	public void setRoot(NodeAVLTree<K, V, F, H> root) {
 		this.root = root;
 	}
 
@@ -23,20 +23,41 @@ public class AVLTree<K, V, F> implements IAVLTree<K, V, F>{
 
 	@Override
 	public boolean addNode(K key, V object, F balanced) {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
 	@Override
 	public boolean deleteNode(K key, V object, F balanced) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public NodeAVLTree<K, V, F> searchNode(K key, V object, F balanced) {
-		// TODO Auto-generated method stub
+	public NodeAVLTree<K, V, F, H> searchNode(K key, V object, F balanced) {
 		return null;
+	}
+
+	@Override
+	public int height(NodeAVLTree<K, V, F, H> node) {
+		int height = 0;
+		if(node != null) {
+			height = (int) node.getHeight();
+		}
+		return height;
+	}
+
+	@Override
+	public int nodeMax(int nodeHeightA, int nodeHeightB) {
+		return (nodeHeightA > nodeHeightB) ? nodeHeightA : nodeHeightB;
+	}
+
+	@Override
+	public int balanceTree(NodeAVLTree<K, V, F, H> node) {
+		int balance = 0;
+		if(node != null) {
+			balance = height(node.getLeft()) - height(node.getRight());
+		}
+		return balance;
 	}
 	
 }
--- src/dataStructures/IAVLTree.java
+++ src/dataStructures/IAVLTree.java
@@ -1,9 +1,12 @@
 package dataStructures;
 
-public interface IAVLTree<K, V, F> {
+public interface IAVLTree<K, V, F, H> {
 	
 	public void createTree();
 	public boolean addNode(K key, V object, F balanced);
 	public boolean deleteNode(K key, V object, F balanced);
-	public NodeAVLTree<K, V, F> searchNode(K key, V object, F balanced);
+	public int height(NodeAVLTree<K, V, F, H> node);
+	public int nodeMax(int nodeHeightA, int nodeHeightB);
+	public int balanceTree(NodeAVLTree<K, V, F, H> node);
+	public NodeAVLTree<K, V, F, H> searchNode(K key, V object, F balanced);
 }
