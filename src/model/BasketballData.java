67d021db51fe28be96aaef437bcaf00bc038a733
Gianni <70019070+G20-00@users.noreply.github.com>
2021-10-26 20:43:30 -0500

    change

--- src/model/BasketballData.java
+++ src/model/BasketballData.java
@@ -9,41 +9,75 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-
+import java.util.ArrayList;
 
 public class BasketballData {
-	public static final String NODES = "data/node.txt";
-	public BasketballData() {}
-	
-	public void searchPlayer() {}
-	
+	public static final String TREE_POINST = "data/node_poinst.txt";
+	public static final String TREE_ASSISTANCE = "data/node_assistance.txt";
+	public static final String TREE_BLOCK = "data/node_block.txt";
+	public static final String TREE_POINTS = "data/node_points.txt";
+	public ArrayList<Players> players ;
+	public BasketballData() {
+	}
 
-	
+	public void searchPlayer() {
+	}
+
 	public void importData(String fileName) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new FileReader(fileName));
 		String line = br.readLine();
-		
-		while(line != null) {
+
+		while (line != null) {
 			String[] players = line.split(";");
-			
-			//createPlayer(players);
-			
+
+			// createPlayer(players);
+
 			line = br.readLine();
 		}
 		br.close();
 	}
+
 	public void saveData() throws FileNotFoundException, IOException {
-		ObjectOutputStream ob = new ObjectOutputStream(new FileOutputStream(NODES));
-	//	ob.writeObject(nodoooo);
-		ob.close();
+		// ObjectOutputStream ob = new ObjectOutputStream(new FileOutputStream(NODES));
+		// ob.writeObject(nodoooo);
+		// ob.close();
 	}
+
 	public void loadData() throws FileNotFoundException, IOException, ClassNotFoundException {
-		File f = new File(NODES);
-		if (f.exists()) {
-			ObjectInputStream ob = new ObjectInputStream(new FileInputStream(f));
-			//ARbol = (noooddooo ) ob.readObject();
-			ob.close();
-		}
+		// File f = new File(NODE);
+		// if (f.exists()) {
+		// ObjectInputStream ob = new ObjectInputStream(new FileInputStream(f));
+		// ARbol = (noooddooo ) ob.readObject();
+		// ob.close();
 	}
+
+	public ArrayList<Players> searchArrayMin(int code) {
+		ArrayList<Players> player= new ArrayList<Players>();
+		for (int i = 0; i < players.size(); i++) {
+			if (code <=  players.getTheft()) {
+				player.add(players.get(i));
+			}
+		}
+		return player;
+	}
+	public ArrayList<Players> searchArrayMax(int code) {
+		ArrayList<Players> player= new ArrayList<Players>();
+		for (int i = 0; i < players.size(); i++) {
+			if (code >=  players.getTheft()) {
+				player.add(players.get(i));
+			}
+		}
+		return player;
+	}
+	public ArrayList<Players> searchArrayEquals(int code) {
+		ArrayList<Players> player= new ArrayList<Players>();
+		for (int i = 0; i < players.size(); i++) {
+			if (code ==  players.getTheft()) {
+				player.add(players.get(i));
+			}
+		}
+		return player;
+	}
+
 }
